cbfc0cc4351d2d78c837aefee4d44e26188d5863
Jiwon Kim <kittyvision@gmail.com>
2015-04-25 16:17:08 -0700

    Whole thing runs at ~41 fps instead of ~15 fps

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 $(document).ready(function() { var canvas = document.getElementById('lavalamp-canvas');
+
+    // CONSTANTS
+    var colorStop0 = {r: 255, g: 0, b: 0, a: 255};
+    var colorStop1 = {r: 255, g: 150, b: 0, a: 200};
+
+
+    // SETUP CANVAS2D API, START RENDERING
     var context = canvas.getContext('2d');
     context.translate(0.5, 0.5);
-
     var hiddenCanvas = document.getElementById('hidden-canvas');
     var hiddenContext = canvas.getContext('2d');
+    var w = canvas.width;
+    var h = canvas.height;
+    var imageData = context.createImageData(w, h);
+    requestAnimationFrame(frame);
 
+    // LAVA LAMP STATE
     var blobs = [
         blob(0.3, 0.1),
         blob(0.6, 0.075),
@@ -13,7 +24,6 @@ $(document).ready(function() { var canvas = document.getElementById('lavalamp-ca
         blob(0.2, 0.05),
     ];
 
-    requestAnimationFrame(frame);
 
     function frame() {
         physics();
@@ -28,31 +38,21 @@ $(document).ready(function() { var canvas = document.getElementById('lavalamp-ca
     }
     
     function render() {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        var imageData = context.createImageData(canvas.width, canvas.height);
+        context.clearRect(0, 0, w, h);
 
-
-        for (var bidx = 0; bidx < blobs.length; bidx++) {
-            blobs[bidx].render();
-        }
-
-        for (var x = 0; x < canvas.width; x++) {
-            for (var y = 0; y < canvas.height; y++) {
+        for (var y = 0; y < h; y++) {
+            for (var x = 0; x < w; x++) {
                 var sum = 0;
-                
                 for (var bidx = 0; bidx < blobs.length; bidx++) {
                     sum += blobs[bidx].m(x, y);
                 }
-
-                sum /= blobs.length;
-                
-                if (sum > 0.002) {
-                    var pixelIdx = (canvas.width * y + x) * 4;
-                    imageData.data[pixelIdx] = 0;
-                    imageData.data[pixelIdx + 1] = 0;
-                    imageData.data[pixelIdx + 2] = 0;
-                    imageData.data[pixelIdx + 3] = 255;
-                }
+                var pixelIdx = (w * y + x) * 4;
+                var d0 = (0.015 - sum) * 200;
+                var d1 = 1 - d0;
+                imageData.data[pixelIdx] = d1 * colorStop1.r + d0 + colorStop0.r;
+                imageData.data[pixelIdx + 1] = d1 * colorStop1.g + d0 + colorStop0.g;
+                imageData.data[pixelIdx + 2] = d1 * colorStop1.b + d0 + colorStop0.b;
+                imageData.data[pixelIdx + 3] = d1 * colorStop1.a + d0 + colorStop0.a;
             }
         }
         context.putImageData(imageData, 0, 0);
@@ -67,32 +67,18 @@ $(document).ready(function() { var canvas = document.getElementById('lavalamp-ca
             _r2 = r*r;
 
         // center of metaball, in pixel coords
-        var _px = _x * canvas.width,
-            _py = _y * canvas.height;
+        var _px = Math.floor(_x * w + 0.5),
+            _py = Math.floor(_y * h + 0.5);
 
         // origin of the potential map of the metaball, in pixel coords
-        var _opx = _px - canvas.width,
-            _opy = _py - canvas.height; 
-        var _pr = _r * (canvas.width + canvas.height) / 2,
+        var _opx = _px - w,
+            _opy = _py - h; 
+        var _pr = _r * (w + h) / 2,
             _pr2 = _pr*_pr;
         var _mass = Math.PI * Math.pow(2 * _r, 3) * 3/4;
 
         // initial velocity
         var _vy = 0;
-
-        var _potentialMap = [];
-        _computePotentialMap();
-
-        function _computePotentialMap() {
-            for (var x = 0; x < canvas.width * 2; x++) {
-                _potentialMap[x] = new Float32Array(canvas.height * 2);
-                for (var y = 0; y < canvas.height * 2; y++) {
-                    var dx2 = (x - canvas.width)*(x - canvas.width);
-                    var dy2 = (y - canvas.height)*(y - canvas.height);
-                    _potentialMap[x][y] = _pr / (dx2 + dy2);
-                }
-            }
-        }
 
         /** Computes acceleration in the y direction */
         function _ay() {
@@ -104,31 +90,17 @@ $(document).ready(function() { var canvas = document.getElementById('lavalamp-ca
             _y += _vy * dt;
 
             // update px, py, pr
-            _px = _x * canvas.width;
-            _py = _y * canvas.height;
-            _opx = _px - canvas.width;
-            _opy = _py - canvas.height;
-            /*
-            if (_atBottom()) {
-                _morph();
-            }
-    */
-        }
-
-        function _atBottom() {
-        }
-
-        function _morph() {
+            _px = Math.floor(_x * w + 0.5);
+            _py = Math.floor(_y * h + 0.5);
+            _opx = _px - w;
+            _opy = _py - h;
         }
 
         /** The metaball equation */
-        function _m(px1, py1) {
-            var xi = Math.floor(px1 - _opx);
-            var yi = Math.floor(py1 - _opy);
-            if (xi < 0 || yi < 0 || xi >= canvas.width * 2 || yi >= canvas.height * 2) {
-                return 0;
-            }
-            return _potentialMap[xi][yi];
+        function _m(px, py) {
+            var dpx = px - _px;
+            var dpy = py - _py;
+            return _pr / (dpx * dpx + dpy * dpy);
         }
 
         function _render() {
