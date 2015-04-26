8c2bc5b4744b2be575bc84e5651db6ef8fabbe10
Jiwon Kim <kittyvision@gmail.com>
2015-04-25 23:11:13 -0700

    Cleaned up code, added schemes as seperate json to import

--- script.js
+++ script.js
@@ -1,67 +1,75 @@
 $(document).ready(function() {
     var canvas = document.getElementById('lavalamp-canvas');
-
-    // CONSTANTS
-    var SUM_MULTIPLIER = 200;
-    var colorStopsList = [
-        [
-            {force: 1.89, color: {r: 0, g: 0, b: 0, a: 0}},
-            {force: 1.9, color: {r: 255, g: 255, b: 0, a: 100}},
-            {force: 2, color: {r: 255, g: 180, b: 0, a: 200}},
-            {force: 3, color: {r: 255, g: 10, b: 0, a: 255}}
-        ],
-        [
-            {force: 1.89, color: {r: 0, g: 0, b: 0, a: 0}},
-            {force: 2, color: {r: 200, g: 0, b: 255, a: 200}},
-            {force: 3, color: {r: 200, g: 0, b: 255, a: 255}}
-        ],
-        [
-            {force: 1.89, color: {r: 0, g: 0, b: 0, a: 0}},
-            {force: 1.9, color: {r: 150, g: 50, b: 150, a: 100}},
-            {force: 2.3, color: {r: 200, g: 50, b: 200, a: 150}},
-            {force: 2.9, color: {r: 100, g: 150, b: 255, a: 200}},
-            {force: 3, color: {r: 0, g: 255, b: 255, a: 255}}
-        ]
-    ];
-    var colorStopIdx = 0;
-
-    // SETUP CANVAS2D API, START RENDERING
     var context = canvas.getContext('2d');
     context.translate(0.5, 0.5);
 
+    $.getJSON('schemes.json', function(schemes) {
+        initLavalamp(canvas, context, schemes);
+    });
+});
+
+function initLavalamp(canvas, context, schemes) {
+    var dt = 0.1;
+    var lamp = lavalamp(canvas, context, schemes);
+    requestAnimationFrame(frame);
+
+    $('body').click(function() {
+        lamp.toggleScheme();
+    });
+
+    function frame() {
+        lamp.update(dt);
+        lamp.render();
+        requestAnimationFrame(frame);
+    }
+}
+
+function lavalamp(canvas, context, schemes) {
+
+    // CONSTANTS
+    var SUM_MULTIPLIER = 200;
+    var DEFAULT_SCHEME = {
+        bgcolor: '#333',
+        colorStops: [
+            {force: 1.89, color: {r: 0, g: 0, b: 0, a: 0}},
+            {force: 2.1, color: {r: 93, g: 191, b: 140, a: 255}},
+            {force: 3, color: {r: 190, g: 214, b: 140, a: 255}},
+            {force: 5, color: {r: 240, g: 140, b: 110, a: 255}},
+        ]
+    };
+
+    // canvas width and height
     var w = canvas.width;
     var h = canvas.height;
+
+    // viewport width and height
     var vw = w;
     var vh = h * 0.9;
 
+    // image data used to render blobs
     var imageData = context.createImageData(w, h);
 
     // LAVA LAMP STATE
     var blobs = [
         blob(0.2, 0.05),
         blob(0.35, 0.04),
-        blob(0.3, 0.04),
+        blob(0.3, 0.042),
         blob(0.5, 0.1),
         blob(0.6, 0.075),
-        blob(0.8, 0.03),
+        blob(0.8, 0.031),
         blob(0.7, 0.03),
         blob(0.9, 0.09),
     ];
+    var schemeIdx = 0;
 
-    $('body').click(function() {
-        colorStopIdx = (colorStopIdx + 1) % colorStopsList.length;
-    });
-    requestAnimationFrame(frame);
-
-    function frame() {
-        physics();
-        render();
-        requestAnimationFrame(frame);
+    function toggleScheme() {
+        schemeIdx = (schemeIdx + 1) % schemes.length;
+        $('#lavalamp-canvas').css('background', schemes[schemeIdx].background);
     }
 
-    function physics() {
+    function update(dt) {
         for (var bidx = 0; bidx < blobs.length; bidx++) {
-            blobs[bidx].tick(0.1);
+            blobs[bidx].tick(dt);
         }
     }
     
@@ -87,8 +95,14 @@ $(document).ready(function() {
         context.putImageData(imageData, 0, 0);
     }
 
+    /**
+     * Returns a color, in rgba [0, 256), for a point experiencing
+     * the given force
+     *
+     * @param {float} force
+     */
     function colorAt(force) {
-        var colorStops = colorStopsList[colorStopIdx];
+        var colorStops = schemes[schemeIdx].colorStops;
         for (var i = 0; i < colorStops.length; i++) {
             if (force > colorStops[i].force) {
                 continue;
@@ -113,23 +127,6 @@ $(document).ready(function() {
             t0 = (force - cs0.force) / (cs1.force - cs0.force);
             t1 = 1 - t0;
 
-/*
-            console.log("force: ",force);
-            console.log("cs0: ");
-            console.log(cs0);
-            console.log("cs1: ");
-            console.log(cs1);
-            console.log("t0: ",t0);
-            console.log("t1: ",t1);
-            console.log("t1 * cs0.color.r: ", t1 * cs0.color.r);
-            console.log("t1 * cs0.color.g: ", t1 * cs0.color.g);
-            console.log("t1 * cs0.color.b: ", t1 * cs0.color.b);
-            console.log("t1 * cs0.color.a: ", t1 * cs0.color.a);
-            console.log("t0 * cs1.color.r: ", t0 * cs1.color.r);
-            console.log("t0 * cs1.color.g: ", t0 * cs1.color.g);
-            console.log("t0 * cs1.color.b: ", t0 * cs1.color.b);
-            console.log("t0 * cs1.color.a: ", t0 * cs1.color.a);
-*/
             return {
                 r: t1 * cs0.color.r + t0 * cs1.color.r,
                 g: t1 * cs0.color.g + t0 * cs1.color.g,
@@ -141,31 +138,35 @@ $(document).ready(function() {
         return colorStops[colorStops.length - 1].color;
     }
 
+    /**
+     * Represents a blob in the lavalamp.
+     * @param {float} x - initial x position for the blob, [0, 1]
+     * @param {float} r - radius for the blob, [0, 1]
+     */
     function blob(x, r) {
         var LIFTING_FORCE = 3;
 
         var _x = x,
             _y = 1.0,
-            _r = r,
-            _r2 = r*r;
+            _r = r;
 
         // center of metaball, in pixel coords
         var _px = Math.floor(_x * vw + 0.5),
             _py = Math.floor(_y * vh + 0.5);
 
-        var _pr = _r * (vw + vh) / 2,
-            _pr2 = _pr*_pr;
+        var _pr = _r * (vw + vh) / 2;
         var _mass = Math.PI * Math.pow(2 * _r, 3) * 3/4;
 
         // initial velocity
         var _vy = 0;
 
-        /** Computes acceleration in the y direction */
+        // Computes acceleration in the y direction
         function _ay() {
             return -LIFTING_FORCE * (_y - 0.5) / 0.5 * _mass;
         }
 
-        function _tick(dt) {
+        // Updates the position of the blob for dt
+        function tick(dt) {
             _vy += dt * _ay();
             _y += _vy * dt;
 
@@ -174,25 +175,19 @@ $(document).ready(function() {
             _py = Math.floor(_y * vh + 0.5);
         }
 
-        /** The metaball equation */
-        function _m(px, py) {
+        // The metaball equation
+        function m(px, py) {
             var dpx = px - _px;
             var dpy = py - _py;
             return _pr / (dpx * dpx + dpy * dpy);
         }
 
-        function _render() {
-            context.beginPath();
-            context.arc(_px, _py, _pr, 0, 2 * Math.PI, false);
-            context.strokeStyle = 'green';
-            context.stroke(); 
-            context.closePath();
-        }
-
-        return {
-            m: _m,
-            tick: _tick,
-            render: _render
-        }
+        return {m: m, tick: tick}
     }
-});
+
+    return {
+        toggleScheme: toggleScheme,
+        update: update,
+        render: render
+    }
+}
